93f9a5dde1efe11556cb4b2c8867eb27059a999c
Rumaisa008 <rumaisaahmedali821@gmail.com>
2025-05-21 13:55:37 +0500

    Add files via upload

---
+++ assignment-04/Assignments 1 to 6/Hangman/main.py
@@ -0,0 +1,23 @@
+
+
+
+import random
+from words import words
+from hangman_visual import lives_visual_dict
+import string
+
+
+def get_valid_word(words):
+    word = random.choice(words)  # randomly chooses something from the list
+    while '-' in word or ' ' in word:
+        word = random.choice(words)
+
+    return word.upper()
+
+
+def hangman():
+        print('YAY! You guessed the word', word, '!!')
+
+
+if __name__ == '__main__':
+    hangman()
